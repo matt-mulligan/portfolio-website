71a34a6257436a99dcbd5b42f395a5e65c804892
Matt Mulligan <matt@mattmulligan.net>
2026-02-03 18:00:44 +1100

    update basic layout of template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="layout">
     <aside class="sidebar">
       <div class="sb-spacing-top">SPACING TOP</div>
       <div class="sb-img">
-        <NuxtImg class="profile-pic" format="jpg" src="/profile_picture.jpg">
-        </NuxtImg>
+        <NuxtImg class="profile-pic" format="jpg" src="/profile_picture.jpg"> </NuxtImg>
       </div>
       <div class="sb-nav">NAV</div>
       <div class="sb-spacing-bottom">SPACING BOTTOM</div>
@@ -19,63 +17,61 @@
 </template>
 
 <style>
-/* Page-Level Container Grid */
-.layout {
-  display: grid;
-  grid-template-rows: 100vh;
-  grid-template-columns: 18vw 82vw;
-  grid-template-areas:
-      'sidebar content';
-}
+  /* Page-Level Container Grid */
+  .layout {
+    display: grid;
+    grid-template-rows: 100vh;
+    grid-template-columns: 18vw 82vw;
+    grid-template-areas: 'sidebar content';
+  }
 
-/* Main Element Grids */
-.sidebar {
-  grid-area: sidebar;
-  background-color: darkolivegreen;
+  /* Main Element Grids */
+  .sidebar {
+    grid-area: sidebar;
+    background-color: darkolivegreen;
 
-  display: grid;
-  grid-template-rows: 1.5fr 2fr 5fr 1.5fr;
-  grid-template-columns: 1fr;
-  grid-template-areas:
-    'sb-spacing-top'
-    'sb-img'
-    'sb-nav'
-    'sb-spacing-bottom';
+    display: grid;
+    grid-template-rows: 1.5fr 2fr 5fr 1.5fr;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'sb-spacing-top'
+      'sb-img'
+      'sb-nav'
+      'sb-spacing-bottom';
 
-  border: 3px solid red;
-}
+    border: 3px solid red;
+  }
 
-.content {
-  grid-area: content;
-  border: 3px solid blue;
-}
+  .content {
+    grid-area: content;
+    border: 3px solid blue;
+  }
 
-/* Child Sidebar Classes */
-.sb-spacing-top {
-  grid-area: sb-spacing-top;
-  border: 3px solid black;
-}
+  /* Child Sidebar Classes */
+  .sb-spacing-top {
+    grid-area: sb-spacing-top;
+    border: 3px solid black;
+  }
 
-.sb-img {
-  grid-area: sb-img;
-  border: 3px solid black;
-  justify-items: center;
-  align-items: center;
-}
+  .sb-img {
+    grid-area: sb-img;
+    border: 3px solid black;
+    justify-items: center;
+    align-items: center;
+  }
 
-.profile-pic {
-  border-radius: 50%;
-  max-block-size: 200px;
-}
+  .profile-pic {
+    border-radius: 50%;
+    max-block-size: 200px;
+  }
 
-.sb-nav {
-  grid-area: sb-nav;
-  border: 3px solid black;
-}
+  .sb-nav {
+    grid-area: sb-nav;
+    border: 3px solid black;
+  }
 
-.sb-spacing-bottom {
-  grid-area: sb-spacing-bottom;
-  border: 3px solid black;
-}
-
+  .sb-spacing-bottom {
+    grid-area: sb-spacing-bottom;
+    border: 3px solid black;
+  }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
   vite: {
-    plugins: [
-        tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 
   modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui'],
