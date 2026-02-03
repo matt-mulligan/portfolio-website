da3bd54e858a27ebacbe631e4ca3cc5334797f1e
Matt Mulligan <matt@mattmulligan.net>
2026-02-03 18:00:26 +1100

    update basic layout of template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,17 +1,81 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div class="grid grid-cols-[20%_1fr] min-h-screen">
-    <aside class="bg-gray-900 text-white p-4">
-      <h2 class="text-xl font-semibold mb-6">Matt Mulligan</h2>
-
-      <nav class="space-y-2">
-        <NuxtLink to="/" class="block hover:bg-gray-800 rounded p-2">
-          Bio
-        </NuxtLink>
-      </nav>
+  <div class="layout">
+    <aside class="sidebar">
+      <div class="sb-spacing-top">SPACING TOP</div>
+      <div class="sb-img">
+        <NuxtImg class="profile-pic" format="jpg" src="/profile_picture.jpg">
+        </NuxtImg>
+      </div>
+      <div class="sb-nav">NAV</div>
+      <div class="sb-spacing-bottom">SPACING BOTTOM</div>
     </aside>
-
-    <main class="p-8 bg-gray-50">
+    <main class="content">
       <slot />
     </main>
   </div>
 </template>
+
+<style>
+/* Page-Level Container Grid */
+.layout {
+  display: grid;
+  grid-template-rows: 100vh;
+  grid-template-columns: 18vw 82vw;
+  grid-template-areas:
+      'sidebar content';
+}
+
+/* Main Element Grids */
+.sidebar {
+  grid-area: sidebar;
+  background-color: darkolivegreen;
+
+  display: grid;
+  grid-template-rows: 1.5fr 2fr 5fr 1.5fr;
+  grid-template-columns: 1fr;
+  grid-template-areas:
+    'sb-spacing-top'
+    'sb-img'
+    'sb-nav'
+    'sb-spacing-bottom';
+
+  border: 3px solid red;
+}
+
+.content {
+  grid-area: content;
+  border: 3px solid blue;
+}
+
+/* Child Sidebar Classes */
+.sb-spacing-top {
+  grid-area: sb-spacing-top;
+  border: 3px solid black;
+}
+
+.sb-img {
+  grid-area: sb-img;
+  border: 3px solid black;
+  justify-items: center;
+  align-items: center;
+}
+
+.profile-pic {
+  border-radius: 50%;
+  max-block-size: 200px;
+}
+
+.sb-nav {
+  grid-area: sb-nav;
+  border: 3px solid black;
+}
+
+.sb-spacing-bottom {
+  grid-area: sb-spacing-bottom;
+  border: 3px solid black;
+}
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>INDEX CONTENT</div>
+</template>
