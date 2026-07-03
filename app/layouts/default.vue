1575bc56fb1d7fd6f9584a52505382853752648e
Matt Mulligan <matt@mattmulligan.net>
2026-07-03 21:14:59 +1000

    updated themes and added consistent colours

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,77 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="layout">
-    <aside class="sidebar">
+  <div class="template-layout">
+    <aside class="template-sidebar">
       <div class="sb-spacing-top">SPACING TOP</div>
       <div class="sb-img">
-        <NuxtImg class="profile-pic" format="jpg" src="/profile_picture.jpg"> </NuxtImg>
+        <NuxtImg
+          class="profile-pic"
+          format="jpg"
+          src="/profile_picture.jpg"
+          alt="Look at that handsome devil!"
+        >
+        </NuxtImg>
       </div>
       <div class="sb-nav">NAV</div>
       <div class="sb-spacing-bottom">SPACING BOTTOM</div>
     </aside>
-    <main class="content">
+    <main class="template-content">
       <slot />
     </main>
   </div>
 </template>
-
-<style>
-  /* Page-Level Container Grid */
-  .layout {
-    display: grid;
-    grid-template-rows: 100vh;
-    grid-template-columns: 18vw 82vw;
-    grid-template-areas: 'sidebar content';
-  }
-
-  /* Main Element Grids */
-  .sidebar {
-    grid-area: sidebar;
-    background-color: darkolivegreen;
-
-    display: grid;
-    grid-template-rows: 1.5fr 2fr 5fr 1.5fr;
-    grid-template-columns: 1fr;
-    grid-template-areas:
-      'sb-spacing-top'
-      'sb-img'
-      'sb-nav'
-      'sb-spacing-bottom';
-
-    border: 3px solid red;
-  }
-
-  .content {
-    grid-area: content;
-    border: 3px solid blue;
-  }
-
-  /* Child Sidebar Classes */
-  .sb-spacing-top {
-    grid-area: sb-spacing-top;
-    border: 3px solid black;
-  }
-
-  .sb-img {
-    grid-area: sb-img;
-    border: 3px solid black;
-    justify-items: center;
-    align-items: center;
-  }
-
-  .profile-pic {
-    border-radius: 50%;
-    max-block-size: 200px;
-  }
-
-  .sb-nav {
-    grid-area: sb-nav;
-    border: 3px solid black;
-  }
-
-  .sb-spacing-bottom {
-    grid-area: sb-spacing-bottom;
-    border: 3px solid black;
-  }
-</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <div>INDEX CONTENT</div>
+  <div>
+    <h1 class="hero-title">
+      <span class="text-primary">Matt </span>
+      <span class="text-secondary">Mulligan</span>
+    </h1>
+
+    <p class="hero-subtitle">
+      <span class="text-primary">Sydney, Australia - </span>
+      <a class="text-secondary" href="mailto:matt@mattmulligan.net">matt@mattmulligan.net</a>
+    </p>
+
+    <p class="text-body">Python/Spark Software Engineer</p>
+  </div>
 </template>
