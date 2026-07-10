4919f8ac1340cc034d488780220da998be4ea5c8
Matt Mulligan <matt@mattmulligan.net>
2026-07-10 19:29:07 +1000

    fix formatting in default layout template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,32 +1,26 @@
 <script setup lang="ts">
-const links = [
-  { label: 'About', to: '/' },
-  { label: 'Experience', to: '/experience' },
-  { label: 'Education', to: '/education' },
-  { label: 'Skills', to: '/skills' },
-]
+  const links = [
+    { label: 'About', to: '/' },
+    { label: 'Experience', to: '/experience' },
+    { label: 'Education', to: '/education' },
+    { label: 'Skills', to: '/skills' },
+  ];
 </script>
 
 <template>
   <div class="template-layout">
-
     <aside class="template-sidebar">
       <div class="sb-spacing-top"></div>
 
       <div class="sb-content">
         <NuxtImg
-            class="profile-pic"
-            format="jpg"
-            src="/profile_picture.jpg"
-            alt="Look at that handsome devil!"
+          class="profile-pic"
+          format="jpg"
+          src="/profile_picture.jpg"
+          alt="Look at that handsome devil!"
         >
         </NuxtImg>
-        <NuxtLink
-            v-for="link in links"
-            :key="link.to"
-            :to="link.to"
-            class="nav-link"
-        >
+        <NuxtLink v-for="link in links" :key="link.to" :to="link.to" class="nav-link">
           {{ link.label }}
         </NuxtLink>
       </div>
